84d7d8d3ae8b4cdab66f5c8e6934d8cc966fd005
tochiman <yuutotochizawa@gmail.com>
2022-06-21 13:27:32 +0900

    complete

--- homework/3426_1.c
+++ homework/3426_1.c
@@ -1,62 +1,66 @@
-/*
-学生番号:3426
-名前：栃澤侑人
 
-～実行結果～
+//学生番号:3426
+//名前：栃澤侑人
 
-～考察～
-*/
 #include <stdio.h>
 
 void main(void){
     //変数用意
     int m_0;    //大きい数値用
-    int n_0;    //小さい数値用
+    int n_0 = 1;    //小さい数値用
     int m_1;
-    int n_1;
-    int lcm;
+    int n_1 = 1;
     int lcm_1;
     int lcm_2;
-    int confirm;
+    int lcm;
 
     //入力部
     printf("大きいほうの数値を入力してください。");
     scanf("%d",&m_0);
+    int start_m = m_0;
+
     printf("小さいほうの数値を入力してください。");
     scanf("%d",&n_0);
+    int start_n = n_0;
 
     for (int i=0;;i++){
-        confirm = n_1 = m_0 % n_0;     //割り切れるか確認（余りの有り無し）
-        //大きい数値が0の場合終了
+        // printf("m_0(%d):%d\n",i,m_0);
+        // printf("n_0(%d):%d\n",i,n_0);
+        if (n_0 == 0){
+            printf("最大公約数は「%d」です。\n",n_1);
+
+            lcm_1 = start_m / n_1;
+            lcm_2 = m_1 / n_1;
+            lcm = lcm_1 * lcm_2 * n_1;
+
+            printf("最小公倍数は「%d」です。",lcm);
+            break;
+        }
+
+        n_1 = m_0 % n_0;     
+        // printf("n_1(%d):%d\n",i,n_1);
+
         if (m_0 == 0){
             printf("大きい数値が「0」のため終了します。");
             break;
         }
-
-        //もしもresult変数が0で余りがないのであれば、n_0を出力して終了する。
-        if (confirm == 0){
+        if (n_1 == 0){
             printf("最大公約数は「%d」です。\n",n_0);
-            //最小公倍数を求める
-            // printf("%d",m_0); 12707
-            lcm_1 = m_0 / n_0;
-            lcm_2 = m_1 / n_0;
-            printf("%d\n",m_1);
+
+            lcm_1 = start_m / n_0;
+            printf("(%d)",lcm_1);
+            lcm_2 = start_n / n_0;
+            printf("(%d)",lcm_2);
             lcm = lcm_1 * lcm_2 * n_0;
+
             printf("最小公倍数は「%d」です。",lcm);
             break;
         }else{
-        //最大公約数を求める
             m_1 = n_0;
             n_0 = m_1 % n_1;
-            if (n_0 == 0){
-                printf("最大公約数は「%d」です。\n",n_1);
-                lcm_1 = m_0 / n_0;
-                lcm_2 = m_1 / n_0;
-                printf("%d\n",m_1);
-                lcm = lcm_1 * lcm_2 * n_0;
-                printf("最小公倍数は「%d」です。",lcm);
-                break;
-            }
+            // printf("n_0(%d):%d\n",i,n_0);
+            m_0 = n_1;
+            printf("%d\n",m_1);
         }
     }
 }
